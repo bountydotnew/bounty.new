d32eb9a574288a0ca7af3bd9508427240079f7a0
grim <75869731+ripgrim@users.noreply.github.com>
2025-07-14 00:43:02 -0700

    chore: create db tables

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -1,4 +1,13 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import * as authSchema from "./schema/auth";
+import * as bountiesSchema from "./schema/bounties";
+import * as profilesSchema from "./schema/profiles";
 
-export const db = drizzle(process.env.DATABASE_DATABASE_URL || "");
+export const db = drizzle(process.env.DATABASE_URL || "", {
+  schema: {
+    ...authSchema,
+    ...bountiesSchema,
+    ...profilesSchema,
+  },
+});
 
