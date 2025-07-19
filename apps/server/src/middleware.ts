475dccd5787f7e41286e4fa02ce4dc4de619e206
grim <75869731+ripgrim@users.noreply.github.com>
2025-07-19 00:06:55 -0700

    Update middleware.ts

--- apps/server/src/middleware.ts
+++ apps/server/src/middleware.ts
@@ -15,7 +15,8 @@ export function middleware(request: NextRequest) {
     "http://grim.local:3000",
     "http://localhost:3000",
     "http://localhost:3001",
-    "http://192.168.1.147:3000"
+    "http://192.168.1.147:3000",
+    "https://app.databuddy.cc"
   ].filter(Boolean);
 
   const origin = request.headers.get('origin');
