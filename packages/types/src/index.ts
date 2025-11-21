a3a472728e3199fb2a56ae238d7f9fc524a6c919
grim <75869731+ripgrim@users.noreply.github.com>
2025-11-20 20:13:19 -0800

    Update index.ts

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,7 +2,6 @@ export * from './auth';
 export * from './billing';
 export * from './command-menu';
 export * from './github';
-export * from './polar';
 export * from './post';
 export * from './profile';
 export * from './realtime';
