b17a826418305a9294066ed466b414b11ff1972b
grim <75869731+ripgrim@users.noreply.github.com>
2025-12-03 02:49:17 -0800

    fixy fixy

--- packages/db/src/migrations/0003_clumsy_iron_lad.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "bounty" ALTER COLUMN "is_featured" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "handle" text;--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "is_profile_private" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "user" DROP COLUMN "last_login_method";--> statement-breakpoint
-ALTER TABLE "user" ADD CONSTRAINT "user_handle_unique" UNIQUE("handle");
